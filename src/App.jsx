09504fa38b1004f1cb8c5626f08df70895d99c08
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-07 13:50:24 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//import './App.css'
+import Formulario from './Formulario.jsx'
+import Listado from './Listado.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>ADMINISTRADOR DE PACIENTES</h1>
+      <div class="container">
+        <div class="row">
+          <Formulario />
+          <Listado />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
