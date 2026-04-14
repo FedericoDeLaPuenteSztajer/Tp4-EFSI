aea284c29cecf150474095b48d545dc48331e0db
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-14 16:00:23 -0300

    crear citas, incompleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 //import './App.css'
+import { useEffect, useState } from 'react';
 import Formulario from './Formulario.jsx'
 import Listado from './Listado.jsx'
 
 function App() {
+   
+  const [lastId, setId]= useState(2);
+  //const [citas, setCitas] = useEffect([{ "id":1, "mascota": "Nina", "duenio": "Martín", "fecha": "2021-08-05", "hora": "08:20", "sintomas": "Le duele la pierna" }]); //Da error, no aparece nada
+
   return (
     <>
       <h1>ADMINISTRADOR DE PACIENTES</h1>
