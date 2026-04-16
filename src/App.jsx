4fa4d47f04dc876136816e75965c5330fdb7a270
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-16 15:51:27 -0300

    funcionalidad crear cita lista

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ function App() {
   const [lastId, setId]= useState(2);
   const [citas, setCitas] = useState([{ "id":1, "mascota": "Nina", "duenio": "Martín", "fecha": "2021-08-05", "hora": "08:20", "sintomas": "Le duele la pierna" }]);
 
+  const CrearCita = (newCita) =>{
+      setCitas(prev => [...prev, newCita]) //
+  }
+
   return (
     <>
       <h1>ADMINISTRADOR DE PACIENTES</h1>
       <div className="container">
         <div className="row">
-          <Formulario />
+          <Formulario lastId={lastId} setId={setId} CrearCita={CrearCita}/>
           <Listado ListaCitas={citas}/>
         </div>
       </div>
--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,16 +1,32 @@
-function Formulario() {
+function Formulario(props) {
+    const submit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const newCita = {
+            "id": props.lastId, 
+            "mascota": data.get("mascota"),
+            "duenio": data.get("duenio"),
+            "fecha": data.get("fecha"),
+            "hora": data.get("hora"),
+            "sintomas": data.get("sintomas")
+        }
+
+        props.setId(props.lastId+1)
+        props.CrearCita(newCita)
+    }
+
     return (
         <div className="one-half column">
             <h2>Crear mi Cita</h2>
-            <form>
+            <form onSubmit={submit}>
                 <label>Nombre Mascota</label>
-                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" value="" />
+                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" required />
                 <label>Nombre Dueño</label>
-                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" value="" />
+                <input type="text" name="duenio" className="u-full-width" placeholder="Nombre dueño de la mascota" required />
                 <label>Fecha</label>
-                <input type="date" name="fecha" className="u-full-width" value="" />
+                <input type="date" name="fecha" className="u-full-width" required />
                 <label>hora</label>
-                <input type="time" name="hora" className="u-full-width" value="" />
+                <input type="time" name="hora" className="u-full-width" required />
                 <label>Sintomas</label>
                 <textarea name="sintomas" className="u-full-width"></textarea>
                 <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
